6f2c52dfe9c3d7e0d1aa4f6fc7b28054497af2e8
hyebin <whgpqls259@naver.com>
2019-12-27 20:25:03 +0900

    Initialize 3 tabs

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs) ;
+        tabLayout.addTab(tabLayout.newTab().setText("TAB-1")) ;
+        tabLayout.addTab(tabLayout.newTab().setText("TAB-2")) ;
+        tabLayout.addTab(tabLayout.newTab().setText("TAB-3")) ;
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int pos = tab.getPosition() ;
+                changeView(pos) ;
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+        }) ;
+
+    }
+
+    private void changeView(int index) {
+        TextView textView1 = (TextView) findViewById(R.id.text1) ;
+        TextView textView2 = (TextView) findViewById(R.id.text2) ;
+        TextView textView3 = (TextView) findViewById(R.id.text3) ;
+
+        switch (index) {
+            case 0 :
+                textView1.setVisibility(View.VISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 1 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.VISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 2 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.VISIBLE) ;
+                break ;
+
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
